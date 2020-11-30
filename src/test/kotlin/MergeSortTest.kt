b00125b53d785975139783d9bca03b801749a455
azaitsev <azaitsev@okko.tv>
2020-11-30 15:28:18 +0300

    fix

--- src/test/kotlin/MergeSortTest.kt
+++ src/test/kotlin/MergeSortTest.kt
@@ -6,56 +6,56 @@ class MergeSortTest {
     private val mergeSort = MergeSort()
     @Test
     fun sortedArray() {
-        var array = intArrayOf(1, 2, 3, 4, 5)
+        val array = intArrayOf(1, 2, 3, 4, 5)
         mergeSort.mergeSort(array, 0, array.size - 1)
         assertArrayEquals(intArrayOf(1, 2, 3, 4, 5), array, "Массив не отсортирован")
     }
 
     @Test
     fun randomArray() {
-        var array = intArrayOf(10, 4, 0, 1, 6, 7, 8, 9, 15, 10)
+        val array = intArrayOf(10, 4, 0, 1, 6, 7, 8, 9, 15, 10)
         mergeSort.mergeSort(array, 0, array.size - 1)
         assertArrayEquals(intArrayOf(0, 1, 4, 6, 7, 8, 9, 10, 10, 15), array, "Массив не отсортирован")
     }
 
     @Test
     fun oneRepeatElement(){
-        var array = intArrayOf(1, 1, 1, 1, 1, 1, 1)
+        val array = intArrayOf(1, 1, 1, 1, 1, 1, 1)
         mergeSort.mergeSort(array, 0, array.size - 1)
         assertArrayEquals(intArrayOf(1, 1, 1, 1, 1, 1, 1), array, "Массив не содержит только повторяющийся элемент")
     }
 
     @Test
     fun emptyArray(){
-        var array = intArrayOf()
+        val array = intArrayOf()
         mergeSort.mergeSort(array,0,0)
         assertArrayEquals(intArrayOf(), array, "Массив не пустой")
     }
 
     @Test
     fun oneElementArray(){
-        var array = intArrayOf(1)
+        val array = intArrayOf(1)
         mergeSort.mergeSort(array,0,array.size - 1)
         assertArrayEquals(intArrayOf(1), array, "Массив содержит не один элемент")
     }
 
     @Test
     fun testArrayWithNegativeNumbers(){
-        var array = intArrayOf(1, -5, 10, 5, -3, 0, 4, 3, -3)
+        val array = intArrayOf(1, -5, 10, 5, -3, 0, 4, 3, -3)
         mergeSort.mergeSort(array,0,array.size - 1)
         assertArrayEquals(intArrayOf(-5, -3, -3, 0, 1, 3, 4, 5, 10), array, "Массив не отсортирован, сортировка не работает с отрицательными числами")
     }
 
     @Test
     fun testAnotherElementAtTheBeginning(){
-        var array = intArrayOf(5, 1, 2, 3, 4)
+        val array = intArrayOf(5, 1, 2, 3, 4)
         mergeSort.mergeSort(array,0,array.size - 1)
         assertArrayEquals(intArrayOf(1, 2, 3, 4, 5), array, "Массив не отсортирован")
     }
 
     @Test
     fun testAnotherElementAtTheEnd(){
-        var array = intArrayOf(2, 3, 4, 5, 1)
+        val array = intArrayOf(2, 3, 4, 5, 1)
         mergeSort.mergeSort(array,0,array.size - 1)
         assertArrayEquals(intArrayOf(1, 2, 3, 4, 5), array, "Массив не отсортирован")
     }
